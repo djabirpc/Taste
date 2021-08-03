04dc4fdfa5305abc212006278fa9261ff5c00ffb
djabirpc <djabir.pc@outlook.fr>
2021-08-04 00:56:13 +0100

    Adding Food Type

---
+++ Taste/Controllers/FoodTypeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodTypeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public FoodTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(new { data = _unitOfWork.FoodType.GetAll() });
+
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return new JsonResult(new { success = false, message = "Error while Deleting" });
+            }
+            _unitOfWork.FoodType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return new JsonResult(new { success = true, message = "Delete Successful" });
+        }
+    }
+}
---
+++ Taste/wwwroot/js/foodType.js
@@ -0,0 +1,62 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/foodType",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "40%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Admin/foodType/upsert?id=${data}" class="btn btn-success text-white" >
+                                    <i class="fas fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white"  onclick=Delete('/api/foodType/'+${data})>
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                    </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
