dbb76a37a2ef1248b83ac086b5685fe20612892e
djabirpc <djabir.pc@outlook.fr>
2021-08-04 22:43:50 +0100

    Adding MenuItem

---
+++ Taste/Controllers/MenuItemController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(new { data = _unitOfWork.MenuItem.GetAll(null,null,"Category,FoodType") });
+
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                var objFromDb = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id);
+                if (objFromDb == null)
+                {
+                    return new JsonResult(new { success = false, message = "Error while Deleting" });
+                }
+                var imagePath = Path.Combine(_hostingEnvironment.WebRootPath, objFromDb.Image.TrimStart('\\'));
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+
+                _unitOfWork.MenuItem.Remove(objFromDb);
+                _unitOfWork.Save();
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult(new { success = false, message = "Error while Deleting" });
+
+            }
+            return new JsonResult(new { success = true, message = "Delete Successful" });
+        
+        
+        }
+    }
+}
---
+++ Taste/wwwroot/js/menuItem.js
@@ -0,0 +1,65 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/menuitem",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Admin/menuitem/upsert?id=${data}" class="btn btn-success text-white" >
+                                    <i class="fas fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white"  onclick=Delete('/api/menuitem/'+${data})>
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                    </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
